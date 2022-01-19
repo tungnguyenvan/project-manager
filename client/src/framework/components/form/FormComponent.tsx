8b2a9291705c4517d37d4aa58780c353b100bc5f
Nguyễn Văn Tùng <nguyentungtungtungnguyen@gmail.com>
2022-01-19 09:15:25 +0700

    style

--- client/src/framework/components/form/FormComponent.tsx
+++ client/src/framework/components/form/FormComponent.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Style from "./form__component.module.scss";
 
 interface IFormComponentProps {
 
 }
 
-const FormComponent: React.FC<IFormComponentProps> = (props) => <form>{props.children}</form>
+const FormComponent: React.FC<IFormComponentProps> = (props) => <form className={Style.form__component}>{props.children}</form>
 
 export default FormComponent;
